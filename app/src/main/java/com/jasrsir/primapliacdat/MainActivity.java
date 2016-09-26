5540cb6d5013996c5229f0bb4795be2e2093e8ac
Juan Antonio Suarez <jasrsir@gmail.com>
2016-09-26 13:29:35 +0200

    Primera App de ACDAT

---
+++ app/src/main/java/com/jasrsir/primapliacdat/MainActivity.java
@@ -0,0 +1,45 @@
+package com.jasrsir.primapliacdat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Date;
+
+/**
+ * @author Juan Antonio Suarez
+ * @version 1.0
+ * Primera app con Acceso a Datos.
+ */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Tenemos que referenciar los métodos y objetos...
+    //Creamos los objetos
+    Button boton;
+    TextView texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boton = (Button) findViewById(R.id.button);
+        texto = (TextView) findViewById(R.id.textView);
+        boton.setOnClickListener(this);
+        actualizar();
+
+    }
+
+    private void actualizar() {
+        texto.setText(new Date().toString());
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == boton)
+            actualizar();
+    }
+}
